358bc5bba7fe291e79031442ef542d0011baa744
Nicholas Dombrosky <ndombrosky@gmail.com>
2021-04-28 11:05:55 -0600

    added tabs to watchlist component and added interpolation to display what movies the users has in their watchilst

--- src/app/components/watch-list/watch-list.component.ts
+++ src/app/components/watch-list/watch-list.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import {WatchListService} from '../../services/watch-list.service';
+import { Observable } from 'rxjs';
+import {User} from '../../interfaces'
+import {ApiDataService} from '../../services/api-data.service'
 
 @Component({
   selector: 'app-watch-list',
@@ -6,10 +11,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./watch-list.component.scss']
 })
 export class WatchListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  getUserVar = []
+  
+  constructor(private watchListService : WatchListService, private db: AngularFirestore, public apiDataService: ApiDataService ) { 
+    
   }
+  
+  ngOnInit(): void {
+    this.watchListService.getUser();
+    this.getUserVar = this.watchListService.getUserVar;
+    
+
+  }
+
+  wack(){
+    console.log(console.log(this.getUserVar))
+  }
+
 
 }
