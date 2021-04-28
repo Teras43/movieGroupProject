6c6bc062a0e4d8519dc4d0a3f342f7836fb23b76
Nicholas Dombrosky <ndombrosky@gmail.com>
2021-04-28 11:38:57 -0600

    currently working on making the current user be the only one that displays on the watch list component.

--- src/app/components/watch-list/watch-list.component.ts
+++ src/app/components/watch-list/watch-list.component.ts
@@ -4,6 +4,7 @@ import {WatchListService} from '../../services/watch-list.service';
 import { Observable } from 'rxjs';
 import {User} from '../../interfaces'
 import {ApiDataService} from '../../services/api-data.service'
+import {DataShareService} from '../../services/data-share.service'
 
 @Component({
   selector: 'app-watch-list',
@@ -12,21 +13,43 @@ import {ApiDataService} from '../../services/api-data.service'
 })
 export class WatchListComponent implements OnInit {
   getUserVar = []
+  currentUser;
+  interestedUser = []
+
   
-  constructor(private watchListService : WatchListService, private db: AngularFirestore, public apiDataService: ApiDataService ) { 
+  constructor(private watchListService : WatchListService, private db: AngularFirestore, public apiDataService: ApiDataService, public dataShareService : DataShareService) { 
+    this.watchListService.getUser();
+    this.getUserVar = this.watchListService.getUserVar;
     
   }
   
   ngOnInit(): void {
-    this.watchListService.getUser();
-    this.getUserVar = this.watchListService.getUserVar;
+    // this.dataShareService.parseUserInfo();
+    // this.currentUser = this.dataShareService.currentUser;
+    this.wack()
+    
     
 
   }
 
-  wack(){
-    console.log(console.log(this.getUserVar))
+  wack = async () => {
+    await this.dataShareService.parseUserInfo();
+    setTimeout(() => {
+      this.currentUser = this.dataShareService.currentUser;
+      console.log(this.currentUser)
+    }) 
+    
   }
+  // // use this! make a new function that runs this.
+  
+  // // await this.getUserVar.forEach(user => {
+  // //   if (user.id === currentUser.uid) {
+  //   interestedUser.push(user)
+  // //       } else {
+  // //         return
+
+  //then console.log(user)
+
 
 
 }
