2a387499b6910dc508a5b38135a64ffc0a7c85ff
Brandon Curtis <Brandcurtis43@gmail.com>
2021-05-04 16:25:34 -0600

    gh-pages build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -44,7 +44,6 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
     RouterModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
-    // FirebaseUIModule.forRoot(firebaseUiAuthConfig),
     AngularFirestoreModule,
     MaterialModule,
     HttpClientModule,
@@ -58,21 +57,18 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
     measurementId: "G-X3PX1SH59R"},
     () => 'movieGroupProject',
     {
-      enableFirestoreSync: true, // enable/disable autosync users with firestore
-      toastMessageOnAuthSuccess: false, // whether to open/show a snackbar message on auth success - default : true
-      toastMessageOnAuthError: false, // whether to open/show a snackbar message on auth error - default : true
-      authGuardFallbackURL: '/loggedout', // url for unauthenticated users - to use in combination with canActivate feature on a route
-      authGuardLoggedInURL: '/loggedin', // url for authenticated users - to use in combination with canActivate feature on a route
-      passwordMaxLength: 60, // `min/max` input parameters in components should be within this range.
-      passwordMinLength: 8, // Password length min/max in forms independently of each componenet min/max.
-      // Same as password but for the name
+      enableFirestoreSync: true,
+      toastMessageOnAuthSuccess: false, 
+      toastMessageOnAuthError: false, 
+      authGuardFallbackURL: '/loggedout',
+      authGuardLoggedInURL: '/loggedin', 
+      passwordMaxLength: 60,
+      passwordMinLength: 8,
       nameMaxLength: 50,
       nameMinLength: 2,
-      // If set, sign-in/up form is not available until email has been verified.
-      // Plus protected routes are still protected even though user is connected.
       guardProtectedRoutesUntilEmailIsVerified: false,
-      enableEmailVerification: false, // default: true
-      useRawUserCredential: true, // If set to true outputs the UserCredential object instead of firebase.User after login and signup - Default: false
+      enableEmailVerification: false,
+      useRawUserCredential: true,
     }),
     
   ],
